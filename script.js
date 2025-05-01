877d794194b209ce6ed9242f4182ed328b392406
Kallam <kallamsamad@gmail.com>
2025-05-01 16:57:29 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Script for toggling the visibility of the banner on mobile when the hamburger is clicked
-document.getElementById('banner-toggle').addEventListener('click', function() {
-    const banner = document.getElementById('black-banner');
+document.querySelector('.banner-toggle').addEventListener('click', function() {
+    const banner = document.querySelector('.black-banner');
     // Toggle the display of the banner when the hamburger menu is clicked
     if (banner.style.display === 'none' || banner.style.display === '') {
         banner.style.display = 'flex';  // Show the banner
