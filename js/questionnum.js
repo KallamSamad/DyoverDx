56d0a3373bac33dce48e14304d89edef7887988b
Kallam <kallamsamad@gmail.com>
2025-05-12 14:04:06 +0100

    Update and rename questionnumjs to questionnum.js

---
+++ js/questionnum.js
@@ -0,0 +1,11 @@
+let score=0
+let truth=false
+questions=[]
+answer=[]
+for (let i = 1; i < 21; i++) {
+  questions.push(i);
+}
+for(const [index, value] of questions.entries()){
+  console.log("Question " + value + ": ")
+              }
+              
