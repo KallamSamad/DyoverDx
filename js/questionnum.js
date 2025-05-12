270b42d8b789c90a31b6ebdf968d76b0e33f497e
Kallam <kallamsamad@gmail.com>
2025-05-12 14:37:15 +0100

    Update questionnum.js

--- js/questionnum.js
+++ js/questionnum.js
@@ -7,9 +7,12 @@ symbols=["+","-","÷", "×"]
 for (let i = 1; i < 21; i++) {
   questions.push(i);
 }
-for(const [index, value] of questions.entries()){
-    let number =Math.floor((Math.random() * 100)+1)
-    const operation = Math.floor(Math.random() * symbols.length)
-  console.log("Question " + value + ": " + number + "𝑥" + symbols[operation]+" " + number+"𝑦")
-              }
+function easy(){
+    for(const [index, value] of questions.entries()){
+        let number =Math.floor((Math.random() * 100)+1)
+        const operation = Math.floor(Math.random() * symbols.length)
+      ask=("Question " + value + ": " + number + "𝑥" + symbols[operation]+" " + number+"𝑦" )
+      console.log(ask)
+    }
+
               
