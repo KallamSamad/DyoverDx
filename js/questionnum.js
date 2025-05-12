bff82d7b43edcc23545ab994a1f089f1086cf6c5
Kallam <kallamsamad@gmail.com>
2025-05-12 15:26:05 +0100

    Update questionnum.js

--- js/questionnum.js
+++ js/questionnum.js
@@ -1,18 +1,38 @@
+// Online Javascript Editor for free
+// Write, Edit and Run your Javascript code using JS Online Compiler
 let score=0
 let truth=false
 questions=[]
 answer=[]
-let difficulty =0
-symbols=["+","-","÷", "×"]
+symbols=["×","-","+"]
+const operation = Math.floor(Math.random() * symbols.length);
 for (let i = 1; i < 21; i++) {
   questions.push(i);
 }
-function easy(){
-    for(const [index, value] of questions.entries()){
-        let number =Math.floor((Math.random() * 100)+1)
-        const operation = Math.floor(Math.random() * symbols.length)
-      ask=("Question " + value + ": " + number + "𝑥" + symbols[operation]+" " + number+"𝑦" )
-      console.log(ask)
+function evaluate(n1, n2, op) {
+    switch(op) {
+        case "+": return n1 + n2 ;
+        case "-": return n1  -  n2 ;
+        case "×": return n1  *  n2 ;
+        ; 
     }
-
-              
+}
+function questionloop(){
+    for(const [index, value] of questions.entries()){
+        let number1 =Math.floor((Math.random() * 10)+1);
+        let number2=Math.floor((Math.random() * 10)+1);
+        let op=symbols[operation]
+        ask = `Question ${value}: ${number1}𝑥 ${op} ${number2}𝑥`
+        console.log(ask)
+        let solution=evaluate(number1,number2,op);
+        let useranswer=prompt("solution?")
+        if (useranswer==solution+ "x"){
+             console.log("correct");
+        }else{
+            console.log("incorrect");
+        }
+        }
+        
+        
+    }
+    questionloop()
