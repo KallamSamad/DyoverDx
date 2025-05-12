13738b8773a233aed6312066460281ca346d4b78
Kallam <kallamsamad@gmail.com>
2025-05-12 14:33:55 +0100

    Update questionnum.js

--- js/questionnum.js
+++ js/questionnum.js
@@ -2,10 +2,14 @@ let score=0
 let truth=false
 questions=[]
 answer=[]
+let difficulty =0
+symbols=["+","-","÷", "×"]
 for (let i = 1; i < 21; i++) {
   questions.push(i);
 }
 for(const [index, value] of questions.entries()){
-  console.log("Question " + value + ": ")
+    let number =Math.floor((Math.random() * 100)+1)
+    const operation = Math.floor(Math.random() * symbols.length)
+  console.log("Question " + value + ": " + number + "𝑥" + symbols[operation]+" " + number+"𝑦")
               }
               
