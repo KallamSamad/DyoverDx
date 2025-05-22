eb6ac5e0af67a4a7040281e4ca3c61543a07d083
Kallam <kallamsamad@gmail.com>
2025-05-22 15:34:40 +0100

    Update quiz3.js

--- Assets/js/quiz3.js
+++ Assets/js/quiz3.js
@@ -11,12 +11,13 @@ symbol=["+","-"]
  }
 for (let i=0; i<20;i++) {
  question.push(i+1);
+}
+
  for (x in question){
     let p= Math.floor(Math.random() * 10 + 1);
     let q = Math.floor(Math.random() * 10 + 1);
     let r = Math.floor(Math.random() * 10 + 1);
     let ans=sum(p,q)
      console.log("Compute: " + p + "("+ q+"x"+"+"+r+")");
-    }
     } 
     
