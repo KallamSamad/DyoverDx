9375a4d2ca5eba0a77c075bbf2b1e2b671094026
Kallam <kallamsamad@gmail.com>
2025-05-22 15:01:12 +0100

    Update quiz3.js

--- Assets/js/quiz3.js
+++ Assets/js/quiz3.js
@@ -2,7 +2,7 @@ let score =0;
 let question=[];
 let symbol = ["+","-"];
 let letter =["x","y","z"];
-for (let i=1; i<21;) {
- question.push(i)
+for (let i=0; i<20;i++) {
+ question.push(i+1);
 }
-}
+ console.log(question);
