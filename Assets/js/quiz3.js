1dceb6e3e8090d6ac5be86c116194e402c7016db
Kallam <kallamsamad@gmail.com>
2025-05-22 15:17:39 +0100

    Update quiz3.js

--- Assets/js/quiz3.js
+++ Assets/js/quiz3.js
@@ -4,10 +4,19 @@ let symbol = ["+","-","*"];
 let letter =["x","y","z"];
 for (let i=0; i<20;i++) {
  question.push(i+1);
- function sum(a,b,symbol){
-     case "*", return (a*b)
-     case "+", return (a*b)
-     case "-", return (a*b)
+ function sum(a,b,op){
+     switch (op) {
+         case "*": return (a*b);
+         case "+": return (a*b);
+         case "-": return (a*b);
  }
+ for (x in question){
+    let p= Math.floor(Math.random() * 10 + 1);
+    let q = Math.floor(Math.random() * 10 + 1);
+    let ans=sum(p,q)
+     console.log("Compute: " + p +"+" + "("+ q+"x"+r+")");
+    }
+     console.log(question);
+    } 
+    
 }
- console.log(question);
