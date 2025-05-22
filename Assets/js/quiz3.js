7396fd6d15d7b22465b61b5a5a57950cee9da636
Kallam <kallamsamad@gmail.com>
2025-05-22 15:29:15 +0100

    Update quiz3.js

--- Assets/js/quiz3.js
+++ Assets/js/quiz3.js
@@ -1,22 +1,22 @@
 let score =0;
 let question=[];
-let symbol = ["+","-","*"];
 let letter =["x","y","z"];
-for (let i=0; i<20;i++) {
- question.push(i+1);
+symbol=["+","-"]
  function sum(a,b,op){
      switch (op) {
          case "*": return (a*b);
          case "+": return (a*b);
          case "-": return (a*b);
+     }
  }
+for (let i=0; i<20;i++) {
+ question.push(i+1);
  for (x in question){
     let p= Math.floor(Math.random() * 10 + 1);
     let q = Math.floor(Math.random() * 10 + 1);
+    let r = Math.floor(Math.random() * 10 + 1);
     let ans=sum(p,q)
-     console.log("Compute: " + p +"+" + "("+ q+"x"+r+")");
+     console.log("Compute: " + p + "("+ q+"x"+"+"+r+")");
     }
-     console.log(question);
     } 
     
-}
