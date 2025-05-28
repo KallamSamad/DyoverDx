38b70183cb2ff63f331f3c2cfe1acabf2edaa98d
Kallam <kallamsamad@gmail.com>
2025-05-28 15:27:52 +0100

    Update quiz3.js

--- Assets/js/quiz3.js
+++ Assets/js/quiz3.js
@@ -4,8 +4,8 @@ let letter =["x","y","z"];
 symbol=["+","-"]
  function sum(a,b,c,op,letter1,letter2){
      switch (op) {
-         case "+": return ((a*b)+letter1)+((a*c)+letter2);
-         case "-": return ((a*b)+letter1)-((a*c)+letter2);
+         case "+": return ((a*b)+letter1)+op+((a*c)+letter2);
+         case "-": return ((a*b)+letter1)+op+((a*c)+letter2);
      }
  }
 for (let i=0; i<20;i++) {
@@ -20,7 +20,7 @@ for (let i=0; i<20;i++) {
     let letter2 = letter[Math.floor(Math.random() * letter.length)];
     let sym1 = symbol[Math.floor(Math.random() * symbol.length)];
 
-    let ans=sum(p,q,r,sym1,letter)
+    let ans=sum(p,q,r,sym1,letter1,letter2)
     let ask=console.log(prompt("Compute: " + p+"("+q+letter1+sym1+r+")")) 
     if (ask ==ans){
      console.log("correct")
