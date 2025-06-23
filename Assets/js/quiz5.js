ee8ce6736da07b2d9b7f8d17b380e88f134810e3
Kallam <kallamsamad@gmail.com>
2025-06-23 18:13:02 +0100

    Update quiz5.js - added let to make variables local

--- Assets/js/quiz5.js
+++ Assets/js/quiz5.js
@@ -3,8 +3,8 @@ let square = [1,   4,   9,  16,  25,36,  49,  64,  81, 100,121, 144, ]
 function disc(){
     let state=false
     while (state==false){
-        b=Math.floor(Math.random()*150)
-        c=Math.floor(Math.random()*150)
+       let  b=Math.floor(Math.random()*150)
+        let c=Math.floor(Math.random()*150)
         if (square.includes(b**2-(4*c))){
             return{
                 p1:b,
