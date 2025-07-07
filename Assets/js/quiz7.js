837853d5e4976bab18f2cc2f9630b58a30c05cb9
Kallam <kallamsamad@gmail.com>
2025-07-07 14:08:30 +0100

    Update quiz7.js

--- Assets/js/quiz7.js
+++ Assets/js/quiz7.js
@@ -7,16 +7,17 @@ function algorithm (){
     let b = Math.floor((Math.random()*2)+2)
     let y = Math.floor((Math.random()*2)+2)
     let z = Math.floor((Math.random()*2)+2)
-    let quest= `\((${a}x+${y})(${b}x+${z})\)`
-    let ans = `\(${a*b}x^2+${(a*z)+(b*y)}x+${y*z}`\)
-    let input=getElementById("input").value
+    let quest= `(${a}x+${y})(${b}x+${z})`
+    let ans = `${a*b}x^2+${(a*z)+(b*y)}x+${y*z}`
+    let input=document.getElementById("input").innerHTML
     if (input ==ans){
         score++
-    else{
-        let corAns=getElementById("output").innerHtml=`Incorrect - the answer is ${ans}`
     }
-    let score =getElementById("score").innerHtml 
-    {
+    else{
+        let corAns=document.getElementById("output").innerHTML=`Incorrect - the answer is ${ans}`
+    }
+    score =document.getElementById("score").innerHTML
+}
 algorithm()
 
 
