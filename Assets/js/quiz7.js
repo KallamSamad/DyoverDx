9932b29183fd2f03f44c947d856e0abc86d993c3
Kallam <kallamsamad@gmail.com>
2025-07-07 15:00:15 +0100

    Update quiz7.js

--- Assets/js/quiz7.js
+++ Assets/js/quiz7.js
@@ -27,3 +27,4 @@ document.getElementById("submitBtn").addEventListener('click', () => {
 if (questionLength==questionIndex){
     let final=document.getElementById("output").innerHTML=`This is the end of the quiz, your score is ${score}/20`
 }
+algorithm()
